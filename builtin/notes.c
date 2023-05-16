15fb05e4537fde69fe65358e6f75d030fd98a7b9
Elijah Newren <newren@gmail.com>
2023-05-16 06:34:06 +0000

    object-store-ll.h: split this header out of object-store.h
    The vast majority of files including object-store.h did not need dir.h
    nor khash.h.  Split the header into two files, and let most just depend
    upon object-store-ll.h, while letting the two callers that need it
    depend on the full object-store.h.
    
    After this patch:
        $ git grep -h include..object-store | sort | uniq -c
              2 #include "object-store.h"
            129 #include "object-store-ll.h"
    
    Diff best viewed with `--color-moved`.
    
    Signed-off-by: Elijah Newren <newren@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/notes.c
+++ builtin/notes.c
@@ -15,7 +15,7 @@
 #include "hex.h"
 #include "notes.h"
 #include "object-name.h"
-#include "object-store.h"
+#include "object-store-ll.h"
 #include "path.h"
 #include "repository.h"
 #include "blob.h"
